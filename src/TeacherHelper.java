e576defe1734bac415267c51e8c3d51489d8cd8a
Ivan de Siqueira <ivandesiqueira@gmail.com>
2021-10-15 21:34:14 -0300

    implementado variavel horaAula

--- src/TeacherHelper.java
+++ src/TeacherHelper.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 public class TeacherHelper {
 
-	public static final double horaAula = 17.50;
+	// public static final double horaAula = 17.50;
 
 	public static void main(String[] args) {
 		int opcao = 0, numeroAulas, qtdeAlunos, i;
-		double salarioBase, horaAtividade, descansoSemanalRemunerado, salario, notaAluno, mediaAlunos;
+		double salarioBase, horaAtividade, descansoSemanalRemunerado, salario, notaAluno, mediaAlunos, horaAula;
 
 		Random gerador = new Random();
 
@@ -31,6 +31,8 @@ public class TeacherHelper {
 				System.out.println(
 						"Para calcular seu salário base precisamos saber quantas aulas semanais o professor tem na instituição");
 				numeroAulas = leitor.nextInt();
+				System.out.println("Por favor, digite o valor da hora aula:");
+				horaAula = leitor.nextDouble();
 				salarioBase = numeroAulas * 4.5 * horaAula;
 				horaAtividade = salarioBase * 0.05;
 				descansoSemanalRemunerado = (salarioBase + horaAtividade) / 6;
